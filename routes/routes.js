e5ca7cf6dd19ee48765e935e9445d864ced82f1a
Clément S <csimonin.dev@outlook.fr>
2018-05-31 00:49:04 +0200

    Download qui marche v0.1

--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,13 @@
 
 const fs = require('fs');
+const fileUpload = require('express-fileupload');
+
 const appRouter = (app) => {
+  app.use(fileUpload());
   
   const sendSeekable = require('send-seekable');  
   app.get("/getData", (req, res) => {
+    console.log('getData')
     /*const apiData = {
       path: 'user1',
       children: [
@@ -117,6 +121,23 @@ const appRouter = (app) => {
         res.sendSeekable(stream, { length : stat.size});
       }); 
     }
+  })
+  app.put("/uploadFile", (req, res) => {
+    if (!req.files || !req.body.path) {
+      return res.status(400).send('Missing file data');
+    }
+ 
+    // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+    const file = req.files.data
+    const path = req.body.path
+        // Use the mv() method to place the file somewhere on your server
+    file.mv(`./datas/${path}`, function(err) {
+      if (err) {
+        console.log(err);
+        return res.status(500).send(err);
+      }  
+      res.send('File uploaded!');
+    });
   })
 }
 

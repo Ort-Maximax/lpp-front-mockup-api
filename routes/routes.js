d5697a345b158d92562ef48b4769a19ad7bbf065
Clément S <csimonin.dev@outlook.fr>
2018-05-31 23:21:11 +0200

    upload + treescript v1

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,7 @@
 
 const fs = require('fs');
 const fileUpload = require('express-fileupload');
+const PythonShell = require('python-shell');
 
 const appRouter = (app) => {
   app.use(fileUpload());
@@ -8,70 +9,7 @@ const appRouter = (app) => {
   const sendSeekable = require('send-seekable');  
   app.get("/getData", (req, res) => {
     console.log('getData')
-    /*const apiData = {
-      path: 'user1',
-      children: [
-        {
-          name: 'Folder 1',
-          children: [
-            {
-              name: 'Folder 11',
-              children: [
-                { name: 'test1.tar.gz' },
-                { name: 'file1.rar' },
-              ],
-            },
-            {
-              name: 'Folder $^3ç#?&',
-              children: [
-                { name: 'test2.tar.gz' },
-                { name: 'file2.rar' },
-              ],
-            },
-            {
-              name: 'Folder qui a un nom qui, il se trouve, est très long et prend beaucoup d\'espace OMG ce nom est interminable',
-              children: [
-                { name: 'test3.tar.gz' },
-                { name: 'file3.rar' },
-              ],
-            },
-            { name: 'file.js', lastUpdated: '01/01/2001' },
-            { name: 'file.html', lastUpdated: '02/02/2002' },
-            { name: 'file.json', lastUpdated: '03/03/2003' },
-    
-            { name: 'file.avi', lastUpdated: '04/04/2004' },
-            { name: 'file.mkv', lastUpdated: '05/05/2005' },
-            { name: 'file.mp4', lastUpdated: '06/06/2006' },
-    
-            { name: 'file.mp3', lastUpdated: '07/07/2007' },
-            { name: 'file.ogg', lastUpdated: '08/08/2008' },
-            { name: 'file.flac', lastUpdated: '09/09/2009' },
-    
-            { name: 'file.jpg', lastUpdated: '10/10/2010' },
-            { name: 'file.jpeg', lastUpdated: '11/11/2011' },
-            { name: 'file.png', lastUpdated: '12/12/2012' },
-    
-          ],
-        },
-        {
-          name: 'Folder 2',
-          children: [
-            {
-              name: 'Folder 21',
-              children: [
-                { name: 'test4.tar.gz' },
-                { name: 'file4.rar' },
-              ],
-            },
-            { name: 'LoneFile.js' },
-          ],
-        },
-        {
-          name: 'Empty Folder',
-          children: [],
-        },
-      ],
-    };*/
+    /*
     const apiData= {
       path: 'user1',
       children: [
@@ -106,8 +44,14 @@ const appRouter = (app) => {
         },
       ],
 
-    }
-    res.status(200).send(apiData);
+    }*/
+
+
+    PythonShell.run('../Tree.py', { args: ['datas/user1'] }, (err, results) => {
+      if (err) throw err;
+      res.status(200).send(results[0]);
+    });
+    
   });
 
   app.get("/getFile", sendSeekable, (req, res) => {
@@ -130,12 +74,12 @@ const appRouter = (app) => {
     // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
     const file = req.files.data
     const path = req.body.path
-        // Use the mv() method to place the file somewhere on your server
+    // Use the mv() method to place the file somewhere on your server
     file.mv(`./datas/${path}`, function(err) {
       if (err) {
         console.log(err);
         return res.status(500).send(err);
-      }  
+      }
       res.send('File uploaded!');
     });
   })
